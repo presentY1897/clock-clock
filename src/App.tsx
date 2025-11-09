162899c4a2400931435da24559fca4caccc79d36
presenty1897 <presenty1897@gmail.com>
2025-11-09 13:28:04 +0900

    feat: init app page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "react-clock/dist/Clock.css";
+import ClockClockPage from "./pages/ClockClockPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ClockClockPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ClockClockPage.tsx
@@ -0,0 +1,12 @@
+import ClockClock from "../components/ClockClock";
+import "./ClockClockPage.css";
+
+const ClockClockPage = () => {
+  return (
+    <div className="page">
+      <ClockClock />
+    </div>
+  );
+};
+
+export default ClockClockPage;
