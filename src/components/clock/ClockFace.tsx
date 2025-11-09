6e0ed9237ec19faaee12c99903b1f392cbaeab8c
presenty1897 <presenty1897@gmail.com>
2025-11-09 13:12:31 +0900

    feat: init clock face with hand

---
+++ src/components/clock/ClockFace.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./ClockFace.css";
+
+type HandProps = {
+  innerSize: string;
+  outerSize: string;
+  width: string;
+};
+
+const Hand: React.FC<HandProps> = ({ innerSize, outerSize, width = "1%" }) => {
+  return (
+    <div
+      className="hand-container"
+      style={{
+        width: `${width}`,
+      }}
+    >
+      <div className="hand-inner-container">
+        <div className="hand" style={{ height: `${innerSize}` }}></div>
+      </div>
+      <div className="hand-outer-container">
+        <div className="hand" style={{ height: `${outerSize}` }}></div>
+      </div>
+    </div>
+  );
+};
+
+type TransitionProps = {
+  transitionDuration: string;
+  transitionTimingFunction?: string;
+};
+
+type ClockFaceProps = {
+  size: string;
+  hourAngle: number;
+  minuteAngle: number;
+  hourHand: HandProps;
+  minuteHand: HandProps;
+} & TransitionProps;
+
+const ClockFace: React.FC<Partial<ClockFaceProps>> = ({
+  size = "100px",
+  hourAngle = 0,
+  minuteAngle = 0,
+  transitionDuration = "1s",
+  transitionTimingFunction = "linear",
+  hourHand = { innerSize: "80%", outerSize: "1%", width: "4%" },
+  minuteHand = { innerSize: "90%", outerSize: "1%", width: "4%" },
+}) => {
+  return (
+    <div className="clock-face-container" style={{ width: size, height: size }}>
+      <div className="clock-face">
+        <div
+          className="clock-hand"
+          style={{
+            transform: `rotate(${hourAngle}deg)`,
+            transitionDuration: `${transitionDuration}`,
+            transitionTimingFunction: `${transitionTimingFunction}`,
+          }}
+        >
+          <Hand
+            innerSize={hourHand.innerSize}
+            outerSize={hourHand.outerSize}
+            width={hourHand.width}
+          />
+        </div>
+        <div
+          className="clock-hand"
+          style={{
+            transform: `rotate(${minuteAngle}deg)`,
+            transitionDuration: `${transitionDuration}`,
+            transitionTimingFunction: `${transitionTimingFunction}`,
+          }}
+        >
+          <Hand
+            innerSize={minuteHand.innerSize}
+            outerSize={minuteHand.outerSize}
+            width={minuteHand.width}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClockFace;
